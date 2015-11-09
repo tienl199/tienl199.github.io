3311485a88d05b1af94c9a5af97bf2f7cc4e3c68
tienl199 <tienl199@newschool.edu>
2015-11-09 22:59:09 +0000

    typeface

---
+++ typeface/script.js
@@ -0,0 +1,4 @@
+
+// for (var i = 0; i < 6; i++) {
+//     $(".vertical-lineA1").append('<div class="vertical-line"><div class="v-thin segA"></div><div class="v-medium segA"></div><div class="v-thick segA"></div></div>')
+// }
